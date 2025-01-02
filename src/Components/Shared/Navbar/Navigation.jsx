081106fe24f833c24791dd0beb215caedb78df30
tamzid620 <tamzid620@gmail.com>
2025-01-02 16:08:42 +0600

    responsive navbar fixed

--- src/Components/Shared/Navbar/Navigation.jsx
+++ src/Components/Shared/Navbar/Navigation.jsx
@@ -5,6 +5,7 @@ import Swal from "sweetalert2";
 import app from "../../../firebase/firebase.config";
 import "./Navbar.css";
 import navimg from "../../../../src/assets/icons/Azraelle-logo-bgless.png";
+import menuImg from "../../../../src/assets/icons/menu.png";
 const auth = getAuth();
 
 const Navigation = () => {
@@ -12,6 +13,11 @@ const Navigation = () => {
   const [darkTheme, setDarkTheme] = useState(false);
   const lightThemeClass = "light-theme";
   const darkThemeClass = "dark-theme";
+  const [menuOpen, setMenuOpen] = useState(false) ;
+
+const handleMenuOpen = () => {
+    setMenuOpen(!menuOpen) ; 
+}
 
   useEffect(() => {
     const body = document.body;
@@ -47,17 +53,18 @@ const Navigation = () => {
   };
 
   return (
-    <nav
+    <nav className="flex justify-center mx-auto">
+    <div
       style={{ fontFamily: "Nunito Sans, serif" }}
-      className="bg-blue-200 fixed top-0 py-1 z-10 w-full shadow-sm shadow-gray-900 
+      className="bg-blue-600 fixed top-0 py-1 z-10 w-full shadow-sm shadow-gray-900 
     xl:max-w-7xl lg:max-w-6xl md:max-w-3xl sm: max-w-sm mx-auto"
     >
       {/* nav for large device  */}
-      <div className="lg:flex justify-between md:hidden sm: hidden">
+      <div className="lg:flex justify-between md:hidden sm: hidden px-4">
         <div>
           <img className="w-[85px]" src={navimg} alt="" />
         </div>
-        <div className="flex gap-44">
+        <div className="flex gap-52">
         {/* ul li section  */}
         <div className=" flex items-center">
           <ul className="flex justify-between gap-10 uppercase font-bold text-sm">
@@ -76,7 +83,7 @@ const Navigation = () => {
           </ul>
         </div>
         {/* dark light login logout button  */}
-        <div className="grid md:grid-cols-2 sm: grid-cols-1 items-center gap-5 ">
+        <div className="flex justify-center items-center gap-5 ">
           <div>
             <button
               onClick={() => setDarkTheme(!darkTheme)}
@@ -114,13 +121,17 @@ const Navigation = () => {
         </div>
       </div>
       {/* nav for small device  */}
-      <div className="lg:hidden md:flex justify-between sm: flex">
+      <div className="lg:hidden md:flex justify-between items-center sm: flex px-3 py-0">
       <div>
           <img className="w-[85px]" src={navimg} alt="" />
         </div>
-        {/* ul li section  */}
-        <div className=" flex justify-center items-center leading-10 text-center">
-          <ul className=" uppercase font-bold text-sm">
+      <div onClick={handleMenuOpen}>
+          <img className="w-[30px] drop-shadow-xl relative" src={menuImg} alt="" />
+        </div>
+        {
+            menuOpen ? 
+        <div  className=" absolute top-20 left-0 flex justify-center items-center text-center bg-blue-600 w-full py-1 border-t-2">
+          <ul className=" uppercase font-bold text-sm leading-[60px]">
             <Link to="/">
               <li>Home</li>
             </Link>
@@ -133,9 +144,45 @@ const Navigation = () => {
             <Link to="/dashboard/myselectedclass">
               <li>Dashboard</li>
             </Link>
+          {/* dark light login logout button  */}
+          <div>
+            <button
+              onClick={() => setDarkTheme(!darkTheme)}
+              className=" text-white underline uppercase font-bold px-2 py-1 rounded-sm"
+            >
+              {darkTheme ? "Light Mode" : "Dark Mode"}
+            </button>
+          </div>
+          <div>
+            {user ? (
+              <>
+                <div className="w-10 rounded-full">
+                  <img
+                    className="w-10 rounded-full"
+                    src={user.photoURL}
+                    alt="User"
+                  />
+                </div>
+                <button
+                  onClick={logOut}
+                  className="text-white underline uppercase font-bold px-2 py-1 rounded-sm"
+                >
+                  Logout
+                </button>
+              </>
+            ) : (
+              <Link to="/login">
+                <button className="text-white underline uppercase font-bold px-2 py-1 rounded-sm">
+                  LogIn
+                </button>
+              </Link>
+            )}
+          </div>
           </ul>
-        </div>
+        </div> : <></>
+        }
       </div>
+    </div>
     </nav>
   );
 };
