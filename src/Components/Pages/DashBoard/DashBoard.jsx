2bd8d3057196e49c805a2569efc175e7c891c2bc
tamzid620 <tamzid620@gmail.com>
2025-01-05 21:25:57 +0600

    dashboard color changed

--- src/Components/Pages/DashBoard/DashBoard.jsx
+++ src/Components/Pages/DashBoard/DashBoard.jsx
@@ -1,79 +1,106 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { FcHome, FcApproval, FcRefresh, FcMoneyTransfer, FcManager, FcReading, FcAddDatabase, } from "react-icons/fc";
+import {
+  FcHome,
+  FcApproval,
+  FcRefresh,
+  FcMoneyTransfer,
+  FcManager,
+  FcReading,
+  FcAddDatabase,
+} from "react-icons/fc";
 import useAdmin from "../../../hooks/useAdmin";
 import useInstructor from "../../../hooks/useInstructor";
 import { Fade } from "react-awesome-reveal";
 import useTitle from "../../../hooks/useTitle";
 
 const DashBoard = () => {
-    useTitle('DashBoard')
+  useTitle("DashBoard");
 
-    const [Admin] = useAdmin();
-    const [Instructor] = useInstructor();
+  const [Admin] = useAdmin();
+  const [Instructor] = useInstructor();
 
-    return (
-        <div>
-            <div className="drawer lg:drawer-open">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col items-center justify-center">
-                    <Outlet></Outlet>
-                    <label htmlFor="my-drawer-2" className="btn bg-blue-300 drawer-button lg:hidden">Open drawer</label>
-
-                </div>
-                <div className="drawer-side">
-                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 h-full  text-lg font-semibold bg-blue-100">
-
-                    <Fade direction="down">
-                        <li><NavLink to="/"><FcHome /> Home </NavLink></li>
-                    </Fade>
-
-                        <div className="divider"></div>
-                        <Fade direction="left">
-                        {
-                            Admin ? (
-                                <>
-
-                                    <li><NavLink to="/dashboard/manageclasses"><FcReading /> Manage Classes </NavLink></li>
-
-                                    <li><NavLink to="/dashboard/manageusers"><FcManager /> Manage Users </NavLink></li>
-
-
-                                </>
-                            )
-                                :
-                                Instructor ? (
-                                    <>
-
-                                        <li><NavLink to="/dashboard/addaclass"><FcAddDatabase /> Add A Class </NavLink></li>
-
-                                        <li><NavLink to="/dashboard/myclasses"><FcApproval /> My Classes </NavLink></li>
-
-
-                                    </>
-                                )
-                                    : (
-                                        <>
-
-                                            <li><NavLink to="/dashboard/myselectedclass"><FcApproval /> MY Selected Classes </NavLink></li>
-
-                                            <li><NavLink to="/dashboard/myenrolledclass"><FcRefresh /> My Enrolled Classes </NavLink></li>
-
-                                            <li><NavLink to="/dashboard/paymenthistory"><FcMoneyTransfer /> Payment History </NavLink></li>
-
-
-                                        </>
-
-                                    )
-                        }
-                    </Fade>
-
-                    </ul>
-
-                </div>
-            </div>
+  return (
+    <div style={{ fontFamily: "Nunito Sans, serif" }} >
+      <div className="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center">
+          <Outlet></Outlet>
+          <label
+            htmlFor="my-drawer-2"
+            className="btn bg-[#c5d5df] text-white drawer-button lg:hidden"
+          >
+            Open drawer
+          </label>
         </div>
-    );
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 h-full  text-lg font-semibold bg-[#004C7F] text-white">
+            <Fade direction="down">
+              <li>
+                <NavLink to="/">
+                  <FcHome /> Home{" "}
+                </NavLink>
+              </li>
+            </Fade>
+
+            <div className="divider"></div>
+            <Fade direction="left">
+              {Admin ? (
+                <>
+                  <li>
+                    <NavLink to="/dashboard/manageclasses">
+                      <FcReading /> Manage Classes{" "}
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to="/dashboard/manageusers">
+                      <FcManager /> Manage Users{" "}
+                    </NavLink>
+                  </li>
+                </>
+              ) : 
+              Instructor ? (
+                <>
+                  <li>
+                    <NavLink to="/dashboard/addaclass">
+                      <FcAddDatabase /> Add A Class{" "}
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to="/dashboard/myclasses">
+                      <FcApproval /> My Classes{" "}
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <NavLink to="/dashboard/myselectedclass">
+                      <FcApproval /> MY Selected Classes
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to="/dashboard/myenrolledclass">
+                      <FcRefresh /> My Enrolled Classes
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to="/dashboard/paymenthistory">
+                      <FcMoneyTransfer /> Payment History
+                    </NavLink>
+                  </li>
+                </>
+              )}
+            </Fade>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashBoard;
--- src/Components/Pages/DashBoard/DashBoardMenu/MySelectedClass/MySelectedClass.jsx
+++ src/Components/Pages/DashBoard/DashBoardMenu/MySelectedClass/MySelectedClass.jsx
@@ -3,96 +3,103 @@ import useSelectClass from "../../../../../hooks/useSelectClass";
 import Swal from "sweetalert2";
 import useTitle from "../../../../../hooks/useTitle";
 
-
 const MySelectedClass = () => {
-    useTitle('MySelectedClass')
-    const [selectClass, refetch] = useSelectClass();
+  useTitle("MySelectedClass");
+  const [selectClass, refetch] = useSelectClass();
 
-    const handleDelete = selectClassItem => {
+  const handleDelete = (selectClassItem) => {
+    Swal.fire({
+      title: "Are you sure?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(
+          `https://azraelle-language-camp-server.vercel.app/classselect/${selectClassItem.email}`,
+          {
+            method: "DELETE",
+          }
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              refetch();
+              Swal.fire(
+                "Deleted!",
+                "Your selected class has been deleted.",
+                "success"
+              );
+            }
+          });
+      }
+    });
+  };
 
-        Swal.fire({
-            title: 'Are you sure?',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        })
-            .then((result) => {
-
-                if (result.isConfirmed) {
-                    fetch(`https://azraelle-language-camp-server.vercel.app/classselect/${selectClassItem.email}`, {
-                        method: 'DELETE'
-                    })
-                        .then(res => res.json())
-                        .then(data => {
-                            if (data.deletedCount > 0) {
-                                refetch();
-                                Swal.fire(
-                                    'Deleted!',
-                                    'Your selected class has been deleted.',
-                                    'success'
-                                )
-                            }
-                        })
-                }
-            })
-    }
-
-
-    return (
-        <div className="w-full ms-10 border rounded-lg p-5">
-            <div className="flex justify-between mb-5">
-                <h2 className="font-semibold text-2xl">My Selected Class</h2>
-            </div>
-            <div>
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        {/* head */}
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th></th>
-                                <th>Subject</th>
-                                <th>Instructor</th>
-                                <th>Available Sits</th>
-                                <th>price</th>
-                                <th>Tool</th>
-                                <th>Tool</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                selectClass.map((selectClassItem, index) => (
-                                    <tr key={selectClassItem._id}>
-
-                                        <th>{index + 1}</th>
-                                        <td><img className="w-[100px] rounded-lg" src={selectClassItem?.class_image} alt="" /></td>
-                                        <td>{selectClassItem?.class_name}</td>
-                                        <td>{selectClassItem?.instructor_name}</td>
-                                        <td>{selectClassItem?.available_seats}</td>
-                                        <td>{selectClassItem?.class_price}</td>
-                                        <th>
-                                            <Link to="/dashboard/payment">
-                                                <button className="btn bg-green-300 ">PAY</button>
-                                            </Link>
-                                        </th>
-                                        <th>
-                                            <button
-                                                onClick={() => handleDelete(selectClassItem)}
-                                                className="btn bg-blue-300 "
-                                            >DELETE</button>
-                                        </th>
-
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+  return (
+    <div className="max-w-full border rounded-lg py-10 bg-white">
+        <h2
+          style={{ fontFamily: "Nunito Sans, serif" }}
+          className="font-bold text-2xl text-center mb-5"
+        >
+          My Selected Class
+        </h2>
+      <div className="flex justify-between mb-5">
+      </div>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+                <th>Subject</th>
+                <th>Instructor</th>
+                <th>Available Sits</th>
+                <th>price</th>
+                <th>Tool</th>
+                <th>Tool</th>
+              </tr>
+            </thead>
+            <tbody>
+              {selectClass.map((selectClassItem, index) => (
+                <tr key={selectClassItem._id}>
+                  <th>{index + 1}</th>
+                  <td>
+                    <img
+                      className="w-[100px] rounded-lg"
+                      src={selectClassItem?.class_image}
+                      alt=""
+                    />
+                  </td>
+                  <td>{selectClassItem?.class_name}</td>
+                  <td>{selectClassItem?.instructor_name}</td>
+                  <td>{selectClassItem?.available_seats}</td>
+                  <td>{selectClassItem?.class_price}</td>
+                  <th>
+                    <Link to="/dashboard/payment">
+                      <button className="btn bg-green-300 ">PAY</button>
+                    </Link>
+                  </th>
+                  <th>
+                    <button
+                      onClick={() => handleDelete(selectClassItem)}
+                      className="btn bg-blue-300 "
+                    >
+                      DELETE
+                    </button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MySelectedClass;
